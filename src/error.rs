cb87c1e0d91bcfe38e6b462a199c42b50c396fda
jntun <justin@jntun.com>
2022-05-08 14:01:52 -0700

    Error: Add new GenericError type
    Adds TickError as an alias over GenericError and changes MaxEntitiesError to be an alias.

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 use std::fmt;
 use std::fmt::Formatter;
 
+/*
 #[derive(Debug)]
 pub struct MaxEntitiesError {
     details: String,
@@ -25,4 +26,32 @@ impl Error for MaxEntitiesError {
     fn description(&self) -> &str {
         &self.details
     }
+} */
+
+#[derive(Debug)]
+pub struct GenericError {
+    details: String,
 }
+
+impl GenericError {
+    pub fn new(msg: &str) -> Self {
+        Self {
+            details: msg.to_string(),
+        }
+    }
+}
+
+impl fmt::Display for GenericError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.details)
+    }
+}
+
+impl Error for GenericError {
+    fn description(&self) -> &str {
+        &self.details
+    }
+}
+
+pub type MaxEntitiesError = GenericError;
+pub type TickError = GenericError;
