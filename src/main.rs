725476307851a41b30831895e7c02b4b323a3842
jntun <justin@jntun.com>
2022-05-16 11:05:58 -0700

    Main: Create a world and add p1 & p2 to it then finally tick()
    Pretty cool to see a tick :^)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
+mod world;
 mod entity;
 mod error;
 mod physics;
 
+use std::cell::RefCell;
+use std::rc::Rc;
+use world::World;
 use physics::vec3::Vec3;
 use entity::human::Human;
 use entity::health::*;
-use crate::error::TickError;
+use crate::entity::Entity;
+
 
 fn main() {
-    let ref mut p1 = Human::new(100, None, Some(String::from("player 1")), None);
-    let ref mut p2 = Human::new(100, Some(50), Some(String::from("player 2")), Some(Vec3::new(10.0, 0.0, 0.0)));
+    let p1 = Human::new(100, None, Some(String::from("player 1")), None);
+    let p2 = Human::new(100, Some(50), Some(String::from("player 2")), Some(Vec3::new(10.0, 0.0, 0.0)));
+    let ref mut world = World::new(None);
+    world.add_entity(Rc::new(RefCell::new(Box::new(p1))));
+    world.add_entity(Rc::new(RefCell::new(Box::new(p2))));
 
-    println!("{} | {}\n{}", p1, p2, "-".repeat(50));
-    p1.attack(p2);
-    println!("{}", "-".repeat(50));
-    println!("{:?}\n\n{:?}", p1, p2);
+    println!("{:?}\n{}", world, "-".repeat(50));
+    world.tick();
 }
