6ce3253d9e8104a76d000ca8fa287d5e7bf6cec4
jntun <justin@jntun.com>
2022-05-21 23:24:15 -0700

    Physics: Rename PhysxStats to PhysxData and add Debug & Display impls

--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -1,5 +1,6 @@
 pub(crate) mod vec3;
 
+use std::fmt::{Display, Debug, Formatter};
 use crate::entity::Entity;
 use self::vec3::Vec3;
 
@@ -10,36 +11,58 @@ pub(crate) trait PhysicsEntity: Entity {
     fn get_pos(&self) -> Vec3;
 }
 
-#[derive(Debug, Copy, Clone)]
-pub(crate) struct PhysxStats {
-    pub momentum: Vec3,
-    pub position: Vec3,
-    pub mass:     f64,
+#[derive(Clone)]
+pub struct PhysxData {
+    mass:     f64,
+    momentum: Vec3,
+    position: Vec3,
 }
 
-impl PhysxStats {
+impl PhysxData {
     pub fn new(mass: f64, position: Option<Vec3>) -> Self {
         match position {
             Some(position) => Self {
+                mass,
                 momentum: Vec3::new(0.0, 0.0, 0.0),
                 position,
-                mass,
             },
             None => Self {
+                mass,
                 momentum: Vec3::new(0.0, 0.0, 0.0),
                 position: Vec3::new(0.0, 0.0, 0.0),
-                mass,
             }
         }
     }
 }
 
-impl Default for PhysxStats {
+impl Default for PhysxData {
     fn default() -> Self {
         Self {
+            mass: 0.0,
             momentum: Vec3::default(),
             position: Vec3::default(),
-            mass: 0.0,
         }
+    }
+}
+
+impl Debug for PhysxData {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("data")
+            .field("mass", &self.mass)
+            .field("momentum", &self.momentum)
+            .field("position", &self.position)
+            .field("effects", &self.effects)
+            .finish()
+    }
+}
+
+impl Debug for dyn PhysicsEntity {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct(self.get_name())
+            .field("x", &self.get_physx_data().position.x())
+            .field("y", &self.get_physx_data().position.y())
+            .field("z", &self.get_physx_data().position.z())
+            .field("effects", &self.get_physx_data().effects)
+            .finish()
     }
 }
