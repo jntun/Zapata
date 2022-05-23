22807debfe77232d0782061d98cec61cd4c04867
jntun <justin@jntun.com>
2022-05-23 12:34:09 -0700

    Error: Remove commented out old MaxEntitiesError

--- src/error.rs
+++ src/error.rs
@@ -2,32 +2,6 @@ use std::error::Error;
 use std::fmt;
 use std::fmt::Formatter;
 
-/*
-#[derive(Debug)]
-pub struct MaxEntitiesError {
-    details: String,
-}
-
-impl MaxEntitiesError {
-    pub fn new(msg: &str) -> Self {
-        Self {
-            details: msg.to_string(),
-        }
-    }
-}
-
-impl fmt::Display for MaxEntitiesError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.details)
-    }
-}
-
-impl Error for MaxEntitiesError {
-    fn description(&self) -> &str {
-        &self.details
-    }
-} */
-
 #[derive(Debug)]
 pub struct GenericError {
     details: String,
