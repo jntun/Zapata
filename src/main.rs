20f73a4341aa8d6b63059ff065259e4affb4ee31
jntun <justin@jntun.com>
2022-05-31 14:01:46 -0700

    Main: Add Hitbox component & implement TrackedComponent creation

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,39 @@
-mod scene;
 mod entity;
 mod error;
 mod physics;
+mod scene;
 
 use crate::{
     entity::*,
-    scene::Scene,
     error::ZapataResult,
     physics::vec3::Vec3,
+    scene::{tracked, Scene},
 };
+use std::{cell::RefCell, rc::Rc};
 
 const EPOCH_COUNT: usize = 1000;
 const HUMAN_COUNT: usize = 20;
-
-
 
 fn main() -> error::ZapataResult {
     let mut scene = Scene::new(None);
 
     for i in 0..HUMAN_COUNT {
-        let mut comp: Vec<Box<dyn component::Component>> = Vec::new();
+        let mut comp: Vec<tracked::TrackedComponent> = Vec::new();
         let x = i as f64;
         comp.append(&mut vec![
-            Box::new(component::health::Health::new((i * 10) as i64, Some((i * 5) as i64))),
-            Box::new(component::physics::Physics::new(21.0, Some(Vec3::new(x, x, x)), None)),
+            tracked::TrackedComponent::Health(Rc::new(RefCell::new(
+                component::health::Health::new((i * 10) as i64, Some((i * 5) as i64)),
+            ))),
+            tracked::TrackedComponent::Physics(Rc::new(RefCell::new(
+                component::physics::Physics::new(21.0, Some(Vec3::new(x, x, x)), None),
+            ))),
+            tracked::TrackedComponent::Hitbox(Rc::new(RefCell::new(component::hitbox::Hitbox(
+                Vec3::new(0.75, 2.0, 0.2),
+            )))),
         ]);
 
         match scene.add_entity(comp) {
-            Ok(e) => (),//println!("entity added {:?}", e),
+            Ok(e) => (), //println!("entity added {:?}", e),
             Err(e) => {
                 println!("failed adding entity: {:?}", e);
             }
@@ -39,7 +45,7 @@ fn main() -> error::ZapataResult {
             print!("\nDone - Ran for {} ticks:\n\t", EPOCH_COUNT);
             println!("{:?}", scene);
             ZapataResult::Success
-        },
+        }
         Err(e) => ZapataResult::from(e),
     }
 }
