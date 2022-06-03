137125c661d833b7e4aa27b00b621796d4a80382
jntun <justin@jntun.com>
2022-06-03 15:06:37 -0700

    Tracked: Move human component list into new human() func

--- src/main.rs
+++ src/main.rs
@@ -18,24 +18,8 @@ fn main() -> error::ZapataResult {
     let mut scene = Scene::new(None);
 
     for i in 0..HUMAN_COUNT {
-        let mut comp: Vec<tracked::TrackedComponent> = Vec::new();
-        let x = i as f64;
-        comp.append(&mut vec![
-            tracked::TrackedComponent::from(component::health::Health::new(
-                (i * 10) as i64,
-                Some((i * 5) as i64),
-            )),
-            tracked::TrackedComponent::from(component::physics::Physics::new(
-                21.0,
-                Some(Vec3::new(x, x, x)),
-                None,
-            )),
-            tracked::TrackedComponent::from(component::collider::Collider(vec![
-                physics::hitbox::Hitbox::new(Vec3::new(1.0, 1.0, 1.0), Vec3::new(2.0, 2.0, 2.0)),
-            ])),
-        ]);
-
-        match scene.add_entity(comp) {
+        let pos = i as f64;
+        match scene.add_entity(scene::tracked::human(Some(Vec3::new(pos, pos, pos)))) {
             Ok(e) => (), //println!("entity added {:?}", e),
             Err(e) => {
                 println!("failed adding entity: {:?}", e);
--- src/scene/tracked.rs
+++ src/scene/tracked.rs
@@ -2,6 +2,7 @@ use {
     crate::{
         entity::{component, component::Component, Entity},
         error::ZapataError,
+        physics::{self, vec3::Vec3},
         scene::Scene,
     },
     std::{cell::RefCell, rc::Rc},
@@ -42,4 +43,14 @@ impl From<component::collider::Collider> for TrackedComponent {
     fn from(component: component::collider::Collider) -> Self {
         Self::Collider(Rc::new(RefCell::new(component)))
     }
+}
+
+pub fn human(position: Option<Vec3>) -> Vec<TrackedComponent> {
+    vec![
+        TrackedComponent::from(component::health::Health::new(100, None)),
+        TrackedComponent::from(component::physics::Physics::new(21.0, position, None)),
+        TrackedComponent::from(component::collider::Collider(vec![
+            physics::hitbox::Hitbox::new(Vec3::new(1.0, 1.0, 1.0), Vec3::new(2.0, 2.0, 2.0)),
+        ])),
+    ]
 }
