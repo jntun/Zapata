8c1d5385e410e095e3fd184c98ce82a6dc02391d
jntun <justin@jntun.com>
2022-06-14 10:07:40 -0700

    Collider: Start implementation of using act_on_all_other_components() to detect collisions

--- src/entity/component/collider.rs
+++ src/entity/component/collider.rs
@@ -13,23 +13,14 @@ pub struct Collider(pub Vec<physics::hitbox::Hitbox>);
 impl Collider {}
 
 impl Component for Collider {
-    fn update(&mut self, entity: Entity, scene: &Scene) -> Result<(), ZapataError> {
+    fn update(&mut self, self_entity: Entity, scene: &Scene) -> Result<(), ZapataError> {
         let collide = |e: &tracked::TrackedComponent| -> bool {
-            match e {
-                tracked::TrackedComponent::Collider(collider) => {
-                    todo!("{:?}", collider)
-                }
-                _ => return false,
-            }
-            true
+            return true;
         };
 
-        return match scene.act_on_component_for_entity(entity, collide) {
-            Ok(()) => Ok(()),
-            Err(e) => Err(ZapataError::FatalError(String::from(format!(
-                "hitbox update failed: {:?}",
-                e
-            )))),
+        return match scene.act_on_all_other_entities(self_entity, collide) {
+            Ok(entities) => Ok(()),
+            Err(e) => Err(e),
         };
     }
 
