a0f83ecb79166af882d78cee78931f656eda247c
jntun <justin@jntun.com>
2022-06-14 10:04:21 -0700

    Entity: Add From<usize> impl

--- src/entity/mod.rs
+++ src/entity/mod.rs
@@ -1,8 +1,6 @@
 pub(crate) mod component;
 
-use std::{
-    fmt::{Debug, Formatter},
-};
+use std::fmt::{Debug, Formatter};
 
 #[derive(PartialEq, Eq, Copy, Clone)]
 pub struct Entity(pub usize);
@@ -19,5 +17,11 @@ impl Entity {
 impl Debug for Entity {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!("Entity#{:?}", self.as_u64()))
+    }
+}
+
+impl From<usize> for Entity {
+    fn from(e: usize) -> Self {
+        Self(e)
     }
 }
