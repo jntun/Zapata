9d5799684ac244dc853312cd0eadbdaa5418da00
jntun <justin@jntun.com>
2023-03-07 20:48:05 -0800

    Scene: Make formatting changes

--- src/scene/scene.rs
+++ src/scene/scene.rs
@@ -6,10 +6,7 @@ use {
             effect::{Duration, Effect},
             vec3::Vec3,
         },
-        scene::{
-            Scene,
-            tracked::TrackedComponent,
-        },
+        scene::{tracked::TrackedComponent, Scene},
     },
     std::{
         fmt::{Debug, Formatter},
@@ -19,7 +16,7 @@ use {
 
 const DEFAULT_NAME: &str = "Zapata";
 
-impl super::Scene {
+impl Scene {
     fn pre_update(&mut self) -> Result<(), ZapataError> {
         Ok(())
     }
@@ -42,7 +39,7 @@ impl super::Scene {
     pub fn update(&mut self) -> Result<(), ZapataError> {
         self.lifetime.start();
         if let Err(e) = self.pre_update() {
-            return Err(e)
+            return Err(e);
         }
         if let Err(e) = self.update_entities() {
             return Err(e);
@@ -55,7 +52,7 @@ impl super::Scene {
     }
 }
 
-impl super::Scene {
+impl Scene {
     pub fn component_list_for_entity(&self, entity: Entity) -> Option<&Vec<TrackedComponent>> {
         self.entities.get(entity.index())
     }
@@ -85,7 +82,11 @@ impl super::Scene {
         if self.lifetime.ticks == 0 || self.lifetime.total_tick_time.is_zero() {
             return None;
         }
-        Some(self.lifetime.total_tick_time.div_f64(self.lifetime.ticks as f64))
+        Some(
+            self.lifetime
+                .total_tick_time
+                .div_f64(self.lifetime.ticks as f64),
+        )
     }
 
     pub fn average_delta_tick(&self) -> time::Duration {
@@ -102,7 +103,7 @@ impl super::Scene {
     }
 }
 
-impl Debug for super::Scene {
+impl Debug for Scene {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.debug_struct(self.get_name())
             .field("ticks", &self.lifetime.ticks)
@@ -114,7 +115,7 @@ impl Debug for super::Scene {
     }
 }
 
-impl Default for super::Scene {
+impl Default for Scene {
     fn default() -> Self {
         Self {
             name: String::from(DEFAULT_NAME),
