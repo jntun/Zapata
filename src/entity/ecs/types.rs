16c8d727b94a3e34facaff8c76a1772137c31222
jntun <justin@jntun.com>
2023-04-19 17:26:57 -0700

    ECS: Change entity type creations to use new component::* paths

--- src/entity/ecs/types.rs
+++ src/entity/ecs/types.rs
@@ -1,7 +1,7 @@
 use crate::{
-    entity::{ecs::ECS, Entity},
+    entity::{component, ecs::ECS, Entity},
     error::ZapataError,
-    physics::vec3::Vec3,
+    physics::Vec3,
 };
 
 // Where "types" of entities are constructed & then returned to use
@@ -14,24 +14,24 @@ impl ECS {
             Err(e) => return Err(e),
         }
 
-        if let Err(e) = self.physics.fill_new_entity(
-            &human,
-            crate::entity::component::physics::Physics::new(21.0, pos, None),
-        ) {
+        if let Err(e) = self
+            .physics
+            .fill_new_entity(&human, component::Physics::new(21.0, pos, None))
+        {
             return Err(e);
         }
 
-        if let Err(e) = self.collider.fill_new_entity(
-            &human,
-            crate::entity::component::collider::Collider::human(),
-        ) {
+        if let Err(e) = self
+            .collider
+            .fill_new_entity(&human, component::Collider::human())
+        {
             return Err(e);
         }
 
-        if let Err(e) = self.health.fill_new_entity(
-            &human,
-            crate::entity::component::health::Health::new(100, Some(75)),
-        ) {
+        if let Err(e) = self
+            .health
+            .fill_new_entity(&human, component::Health::new(100, Some(75)))
+        {
             return Err(e);
         }
 
